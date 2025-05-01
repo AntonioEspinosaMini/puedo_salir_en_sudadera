1eb2c7c0b5da109e4fe7a9c720e287890f277e26
Antonio Espinosa <activecourtsdev@gmail.com>
2025-05-01 14:34:22 +0200

    Dark mode in suggestion list

--- assets/app.js
+++ assets/app.js
@@ -205,13 +205,13 @@ document.addEventListener('DOMContentLoaded', function () {
         if (suggestions.length === 0) return;
 
         const suggestionList = document.createElement('ul');
-        suggestionList.classList.add('suggestions-list', 'bg-white', 'border', 'border-gray-300', 'mt-1');
+        suggestionList.classList.add('suggestions-list', 'bg-white', 'dark:bg-[#2a2a2a]', 'border', 'border-gray-300', 'dark:border-gray-600', 'mt-1');
         suggestionList.innerHTML = ''; // Limpiar las sugerencias anteriores
 
         suggestions.forEach(suggestion => {
             const listItem = document.createElement('li');
             listItem.textContent = suggestion.label; // Mostrar el nombre de la ciudad
-            listItem.classList.add('px-4', 'py-2', 'cursor-pointer', 'hover:bg-gray-100');
+            listItem.classList.add('px-4', 'py-2', 'cursor-pointer', 'hover:bg-gray-100', 'dark:hover:bg-gray-700', 'text-gray-800', 'dark:text-gray-100');
             listItem.addEventListener('click', function () {
                 cityInput.value = suggestion.label; // Establece la ciudad seleccionada en el input
                 hideAutocompleteSuggestions(); // Elimina las sugerencias después de seleccionar
