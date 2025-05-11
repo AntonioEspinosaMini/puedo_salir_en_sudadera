ae6b6639cd80297cabaa6d6f5abe2b5ca9a05e0e
Antonio Espinosa <activecourtsdev@gmail.com>
2025-05-11 11:36:50 +0200

    Add img carousel

--- assets/app.js
+++ assets/app.js
@@ -267,4 +267,50 @@ document.addEventListener('DOMContentLoaded', function () {
             localStorage.setItem('theme', 'dark');
         }
     });
+
+    // ********** Codigo para carrusel *********
+    const images = ["sudadera_roj.png", "sudadera_ver.png", "sudadera_ama.png"];
+    const imageElement = document.getElementById("sweatshirtImage");
+
+    let currentIndex = 0;
+
+    // Cargar imagen guardada si existe
+    const saved = localStorage.getItem("sweatshirtImage");
+    if (saved && images.includes(saved)) {
+        currentIndex = images.indexOf(saved);
+        imageElement.src = `assets/${saved}`;
+    }
+
+    const updateImage = () => {
+        const filename = images[currentIndex];
+        imageElement.src = `assets/${filename}`;
+        localStorage.setItem("sweatshirtImage", filename);
+    };
+
+    updateImage();
+    imageElement.classList.remove("hidden");
+
+    // Swipe handling
+    let startX = 0;
+
+    imageElement.addEventListener("touchstart", (e) => {
+        startX = e.touches[0].clientX;
+    });
+
+    imageElement.addEventListener("touchend", (e) => {
+        const endX = e.changedTouches[0].clientX;
+        const diff = endX - startX;
+
+        // Swipe izquierdo o derecho
+        if (Math.abs(diff) > 30) {
+            if (diff < 0) {
+                // Siguiente
+                currentIndex = (currentIndex + 1) % images.length;
+            } else {
+                // Anterior
+                currentIndex = (currentIndex - 1 + images.length) % images.length;
+            }
+            updateImage();
+        }
+    });
 });
